510dbb7e50ccd3580ab56036fd0e88cda63e80ba
Me <me@me.com>
2026-03-28 23:22:11 +0100

    fix: set Floorp as global default web browser (xdg.mimeApps and BROWSER var)

--- nixos-main/user/floorp/default.nix
+++ nixos-main/user/floorp/default.nix
@@ -6,4 +6,21 @@
     home.packages = with pkgs; [
         floorp-bin
     ];
+
+    # Floorp als absoluten Standard-Browser im System festlegen
+    xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+            "text/html" = "floorp.desktop";
+            "x-scheme-handler/http" = "floorp.desktop";
+            "x-scheme-handler/https" = "floorp.desktop";
+            "x-scheme-handler/about" = "floorp.desktop";
+            "x-scheme-handler/unknown" = "floorp.desktop";
+        };
+    };
+
+    # Umgebungsvariable BROWSER setzen (wird unter anderem von xdg-open gelesen)
+    home.sessionVariables = {
+        BROWSER = "floorp";
+    };
 }
